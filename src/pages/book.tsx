f29c3425a70840bc29d6164d7b454dc3a26790f3
Afsoon <said.atrahouch@gmail.com>
2020-04-26 17:35:30 +0200

    Recover name attr

--- src/pages/book.tsx
+++ src/pages/book.tsx
@@ -35,7 +35,13 @@ const Input = ({
         {required && <span className="ml-1 text-sm text-red-700">*</span>}
       </label>
       <div className="mt-1 relative rounded-md shadow-sm">
-        <input type={type} id={htmlFor} className={className} ref={innerRef} />
+        <input
+          type={type}
+          id={htmlFor}
+          name={htmlFor}
+          className={className}
+          ref={innerRef}
+        />
         {error && (
           <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
             <svg
@@ -84,7 +90,12 @@ const Textarea = ({
         {required && <span className="ml-1 text-sm text-red-700">*</span>}
       </label>
       <div className="mt-1 relative rounded-md shadow-sm">
-        <textarea id={htmlFor} className={className} ref={innerRef} />
+        <textarea
+          id={htmlFor}
+          name={htmlFor}
+          className={className}
+          ref={innerRef}
+        />
         {error && (
           <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
             <svg
