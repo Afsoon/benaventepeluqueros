91a2eb5ef42b7adaa0225476164a6fb4405a4fa4
Afsoon <said.atrahouch@gmail.com>
2020-05-05 18:24:36 +0200

    Valid sitemap xml

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -6,27 +6,25 @@ const globby = require('globby');
 
     // Ignore Next.js specific files (e.g., _app.js) and API routes.
     const pages = await globby(['src/pages/**/*.tsx', '!src/pages/_*.tsx', '!src/pages/api']);
-    const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-            ${pages
-                .map((page) => {
-                    const path = page
-                        .replace('src/', '')
-                        .replace('pages', '')
-                        .replace('.tsx', '')
-                        .replace('.mdx', '');
-                    const route = path === '/index' ? '' : path;
-                    return `
-                        <url>
-                            <loc>${`https://benaventepeluqueros.com${route}`}</loc>
-                            <priority>${route === '' ? '1.00' : '0.80'}</priority>
-                            <changefreq>daily</changefreq>
-                        </url>
-                    `;
-                })
-                .join('')}
-        </urlset>
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xlmns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${pages
+        .map((page) => {
+            const path = page
+                .replace('src/', '')
+                .replace('pages', '')
+                .replace('.tsx', '')
+                .replace('.mdx', '');
+            const route = path === '/index' ? '' : path;
+            return `<url>
+        <loc>${`https://benaventepeluqueros.com${route}`}</loc>
+        <priority>${route === '' ? '1.00' : '0.80'}</priority>
+        <changefreq>daily</changefreq>
+    </url>
+    `;
+        })
+        .join('')}
+</urlset>
     `;
 
     fs.writeFileSync('public/sitemap.xml', sitemap);
