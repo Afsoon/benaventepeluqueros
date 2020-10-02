09ea8374d44c14ecefafa2f863521ca155e2f913
Afsoon <said.atrahouch@gmail.com>
2020-10-02 08:16:42 +0200

    WIP new project structure

---
+++ new/src/routes/book/Book.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let data, helpers;
+
+  // add permalinks to the hook list so we can link to the posts.
+  const hooks = data.hookInterface.map((hook) => ({ ...hook, link: helpers.permalinks.hooks({ slug: hook.hook }) }));
+</script>
+
+<svelte:head>
+  <title>Elder.js Template: Book</title>
+</svelte:head>
+
+<div class="h-screen w-screen bg-red">HELLO</div>
