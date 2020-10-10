469957e221cf6faf6f5bbd13f0f0707073420726
Afsoon <said.atrahouch@gmail.com>
2020-10-10 18:49:13 +0200

    book page

--- new/src/routes/book/Book.svelte
+++ new/src/routes/book/Book.svelte
@@ -1,12 +1,103 @@
-<script>
-  export let data, helpers;
-
-  // add permalinks to the hook list so we can link to the posts.
-  const hooks = data.hookInterface.map((hook) => ({ ...hook, link: helpers.permalinks.hooks({ slug: hook.hook }) }));
-</script>
-
 <svelte:head>
-  <title>Elder.js Template: Book</title>
+  <title>Benavente Peluqueros: Book</title>
 </svelte:head>
 
-<div class="h-screen w-screen bg-red">HELLO</div>
+<main class="w-full flex flex-wrap overflow-y-auto overflow-x-hidden">
+  <div class="w-full relative bg-black-lighter text-white-light xl:max-h-sm">
+    <div class="absolute origin-top-left top-0 left-0 w-full pl-4 pt-8 md:pl-8 md:pt-8 xl:pl-16 xl:pt-16 text-xl z-10">
+      <h3 class="w-15ch text-2xl sm:text-3xl md:text-4xl xl:text-5xl font-cormorant font-medium">
+        Wear your hair with pride
+      </h3>
+      <h1
+        class="font-cormorant font font-medium absolute md:pl-16 pt-4 sm:pt-8 md:pt-12 lg:pt-16 text-3xl sm:text-6xl md:text-6xl xl:text-7xl leading-none inline-block uppercase tracking-widest">
+        Benavente
+        <span class="block w-full">
+          <span class="font-semibold font-montserrat uppercase tabular-nums tracking-normal text-sm sm:text-base"><sup
+              class="">1</sup>
+            /
+            <sub class="mr-2">7</sub></span>
+          Peluqueros</span>
+      </h1>
+    </div>
+    <svg class="absolute top-0 left-1/2 h-full transform -translate-x-1/6 z-0" fill="none" viewBox="0 0 640 784">
+      <defs>
+        <pattern
+          id="9ebea6f4-a1f5-4d96-8c4e-4c2abf658047"
+          x="0"
+          y="0"
+          width="20"
+          height="20"
+          patternUnits="userSpaceOnUse">
+          <rect x="0" y="0" width="3" height="3" class="text-white-dark" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect x="0" class="h-full w-1/3" fill="url(#9ebea6f4-a1f5-4d96-8c4e-4c2abf658047)" />
+    </svg>
+    <img class="float-right w-1/2 object-cover h-full" src="/images/start-photo.jpg" alt="" />
+    <div
+      class="hidden md:font-montserrat md:font-semibold md:block absolute origin-bottom-left bottom-0 left-0 md:pl-8 md:pb-8 xl:pl-16 xl:pb-16 text-lg">
+      <span> Quanto mitrae valiando petreum</span>
+      <a href="#1" alt="" class="font-montserrat underline ml-4 pb-2">more details</a>
+    </div>
+  </div>
+  <div class="pt-16 w-full bg-white-lighter lg:flex lg:justify-center">
+    <iframe
+      src="https://app.acuityscheduling.com/schedule.php?owner=19539723"
+      title="Schedule Appointment"
+      class="w-full lg:max-w-xl"
+      height="500"
+      frameBorder="0"
+      style="height: '500px !important'" />
+    <script async defer src="https://embed.acuityscheduling.com/js/embed.js" type="text/javascript">
+    </script>
+  </div>
+  <footer class="w-full border-t border-white-dark text-white-light mx-16 pt-16 pb-4 flex flex-wrap">
+    <div class="w-full flex flex-col md:flex-row pb-20">
+      <div class="pr-32 pb-16 md:pb-0">
+        <h4 class="text-xl font-cormorant font-semibold">Call us now</h4>
+        <span class="text-3xl font-cormorant font-bold pt-16">+00 740 540 00</span>
+      </div>
+      <div class="flex font-cormorant font-semibold flex-col pb-8 md:pb-0">
+        <h4 class="text-xl font-medium">Opening hours:</h4>
+        <div class="flex flex-col pt-4">
+          <span class="font-montserrat font-bold">Monday - Friday</span>
+          <span class="font-montserrat font-medium">10AM - 8PM</span>
+        </div>
+        <div class="flex flex-col pt-4">
+          <span class="font-montserrat font-bold">Saturday</span>
+          <span class="font-montserrat font-medium">10AM - 2PM</span>
+        </div>
+      </div>
+      <div class="flex font-montserrat font-medium flex-col md:ml-auto">
+        <h4 class="text-xl font-cormorant font-semibold">Localization</h4>
+        <span class="pt-4">Level 13,</span>
+        <span class="pt-2 text-sm">2 Elizabeth St,</span>
+        <span class="pt-2 text-sm">Melbourne, Victoria 30</span>
+        <span class="pt-2 text-sm">Australia</span>
+      </div>
+    </div>
+    <div class="w-full flex flex-col items-center md:flex-row">
+      <span class="font-montserrat font-medium whitespace-no-wrap">© 2020 Benavente Peluqueros</span>
+      <div class="pt-2 md:pt-0 md:ml-auto md:block md:text-center md:mt-auto">
+        <a
+          class="inline-block px-2 py-2 text-white-dark hover:text-white-gray focus:text-white-gray"
+          href="https://www.facebook.com/BenaventePeluqueros"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Página de la peluqueria en Facebook">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path
+              d="M23.998 12c0-6.628-5.372-12-11.999-12C5.372 0 0 5.372 0 12c0 5.988 4.388 10.952 10.124 11.852v-8.384H7.078v-3.469h3.046V9.356c0-3.008 1.792-4.669 4.532-4.669 1.313 0 2.686.234 2.686.234v2.953H15.83c-1.49 0-1.955.925-1.955 1.874V12h3.328l-.532 3.469h-2.796v8.384c5.736-.9 10.124-5.864 10.124-11.853z" /></svg>
+        </a>
+        <a
+          class="inline-block px-2 py-2 text-white-dark hover:text-white-gray focus:text-white-gray"
+          href="https://www.instagram.com/benaventepeluqueros"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="Perfil de de Instagram de la peluquería">
+          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path
+              d="M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913a5.885 5.885 0 001.384 2.126A5.868 5.868 0 004.14 23.37c.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558a5.898 5.898 0 002.126-1.384 5.86 5.86 0 001.384-2.126c.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913a5.89 5.89 0 00-1.384-2.126A5.847 5.847 0 0019.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227a3.81 3.81 0 01-.899 1.382 3.744 3.744 0 01-1.38.896c-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421a3.716 3.716 0 01-1.379-.899 3.644 3.644 0 01-.9-1.38c-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678a6.162 6.162 0 100 12.324 6.162 6.162 0 100-12.324zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405a1.441 1.441 0 01-2.88 0 1.44 1.44 0 012.88 0z" /></svg>
+        </a>
+      </div>
+    </div>
+  </footer>
+</main>
