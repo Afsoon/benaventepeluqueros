c75acc752246dd2bb50adcd5e18340680fbcd5e6
Afsoon <said.atrahouch@gmail.com>
2024-01-19 17:21:19 +0100

    Prices section, maxium width

--- src/navigation.js
+++ src/navigation.js
@@ -18,8 +18,5 @@ export const footerData = {
   schedule: [
     {days: "Martes - Viernes", hours: "10:00 - 19:00"},
     {days: "Sabado", hours: "10:00 - 14:00"}
-  ],
-  footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm bg-[url(https://onwidget.com/favicon/favicon-32x32.png)]"></span>.
-  `,
+  ]
 };
---
+++ src/prices.js
@@ -0,0 +1,43 @@
+export const womenPrices = {
+  title: "Mujer",
+  services: [
+    {name: "Secado", price: "5,00€"},
+    {name: "Corte señoras + Secar", price: "20,00€"},
+    {name: "Corte niña (Hasta 8 años)", price: "10,00€"},
+    {name: "Lavar + Peinar", price: "16,00€"},
+    {name: "Lavar + Peinar largo", price: "20,00€"},
+    {name: "Lavar + Cortar + Peinar", price: "27,00€"},
+    {name: "Lavar + Cortar + Peinar Largo", price: "27,00€"},
+    {name: "Lavar + Secar + Tenacilla", price: "Desde 30,00€"},
+    {name: "Tinte sin amoníaco", price: "Desde 26,00€"},
+    {name: "Decoloración", price: "Desde 20,00€"},
+    {name: "Matices", price: "Desde 15,00€"},
+    {name: "Moldeador", price: "Desde 40,00€"},
+    {name: "Mechas plata, pincel, étc", price: "40,00€"},
+    {name: "Richi", price: "80,00€"},
+    {name: "Alisado Nanolis", price: "Desde 160,00€"},
+    {name: "Tratamientos de hidratación, antioxidante, volumen, étc", price: "15,00€"},
+    {name: "Recogidos", price: "Consultar"},
+    {name: "Tinte cejas", price: "5,00€"},
+    {name: "Hacer cejas", price: "8,00€"},
+    {name: "Retoque cejas", price: "4,00€"}
+  ]
+}
+
+export const menPrices = {
+  title: "Hombre",
+  services: [
+    {name: "Corte caballero", price: "12,00€"},
+    {name: "Corte + Lavar caballero", price: "14,00€"},
+    {name: "Cortar niño (Hasta 8 años)", price: "9,00€"},
+    {name: "Arreglo de barba con máquina", price: "5,00€"},
+    {name: "Tinte sin amoníaco", price: "Desde 24,00€"},
+    {name: "Decolaración", price: "Desde 20,00€"},
+    {name: "Matices", price: "Desde 10,00€"},
+    {name: "Desrizado con Nanonils", price: "Desde 60,00€"},
+    {name: "Mechas", price: "Desde 15,00€"},
+    {name: "Moldeador", price: "Desde 25,00€"},
+    {name: "Retoque cejas", price: "4,00€"},
+    {name: "Hacer cejas", price: "8,00€"}
+  ]
+}
