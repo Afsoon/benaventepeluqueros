d20ece8dbc2f9d02be1f853996892ac9d3ef306c
Afsoon <said.atrahouch@gmail.com>
2024-01-19 17:40:26 +0100

    Black bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,6 +10,12 @@ module.exports = {
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        'black-dark': '#000',
+        'black-ligther': '#181818',
+        'white-ligth': '#FFF',
+        'white-lighter': '#fcfcfc',
+        'white-gray': '#f9f9f9',
+        'white-dark': '#b2b2b2',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
