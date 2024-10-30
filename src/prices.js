106a451cc991eba8dc8f912f6b970be8979064a7
Said Atrahouch <said.atrahouch@gmail.com>
2024-10-30 07:03:17 +0000

    Update price list

--- src/prices.js
+++ src/prices.js
@@ -9,18 +9,18 @@ export const womenPrices = {
     {name: "Lavar + Cortar + Peinar", price: "27,00€"},
     {name: "Lavar + Cortar + Peinar Largo", price: "27,00€"},
     {name: "Lavar + Secar + Tenacilla", price: "Desde 30,00€"},
-    {name: "Tinte sin amoníaco", price: "Desde 26,00€"},
+    {name: "Tinte sin amoníaco", price: "Desde 27,00€"},
     {name: "Decoloración", price: "Desde 20,00€"},
-    {name: "Matices", price: "Desde 15,00€"},
-    {name: "Moldeador", price: "Desde 40,00€"},
-    {name: "Mechas plata, pincel, étc", price: "40,00€"},
+    {name: "Matices", price: "Desde 20,00€"},
+    {name: "Moldeador", price: "Desde 45,00€"},
+    {name: "Mechas plata, pincel, étc", price: "Desde 40,00€"},
     {name: "Richi", price: "80,00€"},
-    {name: "Alisado Nanolis", price: "Desde 160,00€"},
-    {name: "Tratamientos de hidratación, antioxidante, volumen, étc", price: "15,00€"},
+    {name: "Alisado Nanolis", price: "Desde 200,00€"},
+    {name: "Tratamientos de hidratación, antioxidante, volumen, étc", price: "16,00€"},
     {name: "Recogidos", price: "Consultar"},
     {name: "Tinte cejas", price: "5,00€"},
-    {name: "Hacer cejas", price: "8,00€"},
-    {name: "Retoque cejas", price: "4,00€"}
+    {name: "Hacer cejas", price: "10,00€"},
+    {name: "Retoque cejas", price: "5,00€"}
   ]
 }
 
@@ -31,11 +31,11 @@ export const menPrices = {
     {name: "Corte + Lavar caballero", price: "14,00€"},
     {name: "Cortar niño (Hasta 8 años)", price: "9,00€"},
     {name: "Arreglo de barba con máquina", price: "5,00€"},
-    {name: "Tinte sin amoníaco", price: "Desde 24,00€"},
+    {name: "Tinte sin amoníaco", price: "Desde 25,00€"},
     {name: "Decolaración", price: "Desde 20,00€"},
     {name: "Matices", price: "Desde 10,00€"},
     {name: "Desrizado con Nanonils", price: "Desde 60,00€"},
-    {name: "Mechas", price: "Desde 15,00€"},
+    {name: "Mechas", price: "Desde 20,00€"},
     {name: "Moldeador", price: "Desde 25,00€"},
     {name: "Retoque cejas", price: "4,00€"},
     {name: "Hacer cejas", price: "8,00€"}
